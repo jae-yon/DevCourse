7e3f7214eeef575ef04852fc2e4798f189c02287
JaeYoung <kimjaeyoung650@gmail.com>
2024-09-03 15:43:24 +0900

    🐥 EXPRESS 생성기로 프로젝트 생성

--- app.js
+++ app.js
@@ -1,51 +1,41 @@
-const express = require('express')
-const app = express()
-const port = 3000
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const fs = require('fs')
-const jsonFile = fs.readFileSync('./db/sample.json', 'utf8')
-const jsonData = JSON.parse(jsonFile)
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-// 객체 생성
-let db = new Map()
+var app = express();
 
-jsonData.fruits.forEach((element, index) => {
-  db.set(index, element)
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-console.log(db);
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-app.listen(port, () => {
-  console.log(`Server is running on localhost: ${port}`)
-})
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
-app.get('/:id', (req, res) => {
-  // 비구조화를 통해 매개변수 값 변수에 선언
-  let {id} = req.params
-  // 형변환 String > int
-  key = parseInt(id);
-  // 객체 호출
-  if (db.get(key) === undefined) {
-    // 변수 값이 없으면 ...
-    res.json({
-      message : "there is no value",
-    })
-  } else {
-    info = db.get(key)
-    info['id'] = key
-    res.json(info)
-  }
-})
-
-// app.get('/:test', (req, res) => {
-//   // : => url로 매개변수를 전달해주는 것으로 인식한다.
-//   // route/__ 빈칸에 오는 값을 parameter 이라는 변수에 담아준다.
-//   const param = req.params
-// 	res.json({
-// 		test : param.test
-// 	})
-
-//   // 자바스크립트 배열 비구조화
-//   const array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-//   const [num1, num2, , num4, ... num_rest] = array
-// })
+module.exports = app;
--- index.js
+++
@@ -1,9 +0,0 @@
-const server = require('./server');
-const router = require('./router');
-const handler = require('./handler');
-const mariadb = require('./db/connect/mariadb');
-
-// DB 연결
-mariadb.connect();
-
-server.start(router.route, handler.handle);
