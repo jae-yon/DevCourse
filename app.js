23060aae933844b3c23375e26d5595bb7444a6ca
JaeYoung <kimjaeyoung650@gmail.com>
2024-09-04 21:02:57 +0900

    라우터로 API 요청(GET&POST) 받고 응답 결과 호출

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var helloRouter = require('./routes/hello');
+var testRouter = require('./routes/test');
 
 var app = express();
 
@@ -14,26 +16,29 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
+// JSON 형태의 데이터를 해석하는 미들웨어
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/test', testRouter);
 app.use('/users', usersRouter);
+app.use('/hello', helloRouter);
 
-// catch 404 and forward to error handler
+// 404(URL 오입력) 에러 발생시 에러 해들러로 넘김
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// 에러 핸들러
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  // 개발 과정의 에러만 제공
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // 에러 페이지 랜더링
   res.status(err.status || 500);
   res.render('error');
 });
---
+++ routes/test.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var router = express.Router();
+
+// JSON 파일 읽어오기
+const fs = require('fs');
+const jsonFile = fs.readFileSync('./public/json/fruits.json', 'utf8');
+const jsonData = JSON.parse(jsonFile);
+
+let db = new Map()
+// 맵 객체에 JSON데이터 담기
+jsonData.fruits.forEach((element, index) => {
+  db.set(index, element);
+});
+
+router.get('/', (req, res) => {
+  // 객체 전체 조회
+  res.json([...db.values()]);
+});
+
+// 객체 개별(선택) 조회
+router.get('/:id', (req, res) => {
+  // 비구조화를 통해 매개변수 값 변수에 선언
+  let {id} = req.params;
+  id = parseInt(id);
+  // 객체 호출
+  if (db.get(id) == undefined) {
+    res.send("There is no value");
+  } else {
+    let arr = db.get(id);
+    arr['id'] = id;
+    res.json(arr);
+  }
+});
+
+router.post('/', (req, res) => {
+  let id = db.size;
+  // DB 객체에 받아온 값 저장
+  db.set(id, req.body);
+  // 객체 전체 조회
+  console.log(db);
+  // 결과 호출
+  res.json({
+    message : db.get(id).name + " is tasty!"
+  });
+});
+
+module.exports = router;
